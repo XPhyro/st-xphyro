893f5cfed637c9c1fe990a9275a543c790215b8c
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-12-03 02:42:21 +0300

    Update spare fonts

--- config.def.h
+++ config.def.h
@@ -8,8 +8,10 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+    "mono:pixelsize=14:antialias=true:autohint=true",
+    "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+    "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+    "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;

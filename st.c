9b83549803190521b6129600af233bdadf3accc2
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-12-15 04:17:21 +0300

    Apply xclearwin

--- st.c
+++ st.c
@@ -1996,10 +1996,8 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid color j=%d, p=%s\n",
 				        j, p ? p : "(null)");
 			} else {
-				/*
-				 * TODO if defaultbg color is changed, borders
-				 * are dirty
-				 */
+				if (j == defaultbg)
+					xclearwin();
 				redraw();
 			}
 			return;
